46019d6b9b5824a9fbbc95d33ed40868a4ea4222
Morgan Bauer <bauer.morgan@gmail.com>
2011-01-23 20:40:42 -0500

    comments

--- des-tests.lisp
+++ des-tests.lisp
@@ -1,7 +1,6 @@
 (defparameter +test-R0+ #b11110000101010101111000010101010)
 (defparameter m #x0123456789ABCDEF)
 
-
 (defun test-expansion ()
   (= (expansion-permutation +test-R0+)
      #b011110100001010101010101011110100001010101010101))
@@ -27,15 +26,15 @@
 (defun ecb-encrypt-block-test ()
   (and
    (= (ecb-encrypt-block #x8787878787878787 #x0E329232EA6D0D73) 0) ;; from The DES Algorithm Illustrated by J. Orlin Grabbe, initial example
-   (= (ecb-encrypt-block #x0123456789abcdef #x133457799BBCDFF1) #x85E813540F0AB405) ;; worked example
+   (= (ecb-encrypt-block #x0123456789abcdef #x133457799BBCDFF1) #x85E813540F0AB405) ;; ditto, worked example
    (= (ecb-encrypt-block #x02468aceeca86420 #x0f1571c947d9e859) #xda02ce3a89ecac3b) ;; from stallings 5e pg 85 example
-   (= (ecb-encrypt-block #x12468aceeca86420 #x0f1571c947d9e859) #x057cde97d7683f2a) ;; from stallings 5e pg 86 alternate plaintext single bit difference
-   (= (ecb-encrypt-block #x02468aceeca86420 #x1f1571c947d9e859) #xEE92B50606B62B0B) ;; from stallings 5e pg 87 alternate key single bit difference
+   (= (ecb-encrypt-block #x12468aceeca86420 #x0f1571c947d9e859) #x057cde97d7683f2a) ;; from stallings 5e pg 86 alternate plaintext - single bit difference
+   (= (ecb-encrypt-block #x02468aceeca86420 #x1f1571c947d9e859) #xEE92B50606B62B0B) ;; from stallings 5e pg 87 alternate key - single bit difference
    ))
 
 (defun ecb-message-test ()
   (with-output-to-string (*standard-output*)
-    (loop for char across "Your lips are smoother than vaseline"
+    (loop for char across "Your lips are smoother than vaseline" ; example message from DES illustrated
        do (format t "~x" (char-code char)))))
 ;; (list  596F7572206C6970 732061726520736D 6F6F746865722074 68616E2076617365 6C696E65)
 
